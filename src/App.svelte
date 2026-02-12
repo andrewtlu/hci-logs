c20f43db8038743e6e384b79b308c1a53e007df8
andrewtlu <andrewlulu2012@gmail.com>
2026-02-12 11:31:25 -0500

    Remove react, set up vite

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import ControlGroup from "./components/ControlGroup.svelte";
+</script>
+
+<svelte:head>
+  <title>HCI Project Team Portfolio</title>
+</svelte:head>
+
+<main
+  class="w-screen min-h-screen bg-base-100 flex justify-center items-center flex-col"
+>
+  <!-- "nav" -->
+  <header></header>
+
+  <div class="w-187.5 bg-transparent flex justify-center gap-8 flex-col">
+    <!-- title -->
+    <div class="flex flex-col gap-2">
+      <h1 class="text-4xl font-bold pb-1">🍰 NutriCare</h1>
+      <h2>Sumon Kanti Dey, Andrew Lu, & Julian Lucio Paredes</h2>
+      <div class="divider"></div>
+      <ControlGroup />
+    </div>
+
+    <!-- p1, p2, ... -->
+  </div>
+
+  <!-- footer -->
+  <footer
+    class="footer sm:footer-horizontal footer-center bg-transparent text-neutral-content p-10"
+  >
+    <a
+      href="https://github.com/andrewtlu/hci-logs/"
+      class="link link-hover link-primary">GitHub Repository</a
+    >
+  </footer>
+</main>
---
+++ src/components/ControlGroup.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { IconSortAscending, IconSortDescending } from "@tabler/icons-svelte";
+  import { sort } from "../stores/sort.svelte";
+</script>
+
+<!-- @component
+Table of contents for easy navigation.
+ -->
+<div class="flex flex-row gap-1.5 items-center" id="toc">
+  <div class="tooltip tooltip-right">
+    <div class="tooltip-content">
+      {#if sort.ascending}
+        Showing newest first
+      {:else}
+        Showing oldest first
+      {/if}
+    </div>
+    <button
+      onclick={() => {
+        sort.ascending = !sort.ascending;
+      }}
+      aria-label="button"
+      class="btn btn-sm btn-square bg-gray-300 shadow-sm"
+    >
+      {#if sort.ascending}
+        <IconSortAscending />
+      {:else}
+        <IconSortDescending />
+      {/if}
+    </button>
+  </div>
+</div>
