d6b05aa4903b0b54a8c3b107be3bdcae096a33e0
andrewtlu <andrewlulu2012@gmail.com>
2026-02-16 21:27:59 -0500

    Add more P2 content

--- src/components/P2.svelte
+++ src/components/P2.svelte
@@ -1,3 +1,10 @@
+<script>
+  import sofia from "/p2-sofia.jpg";
+  import stefano from "/p2-stefano.jpg";
+  import lucia from "/p2-lucia.jpg";
+  import owen from "/p2-owen.jpg";
+</script>
+
 <!-- @component
 P2 component
  -->
@@ -8,10 +15,31 @@ P2 component
       <p class="pbody">
         Having decided to target the demographic of individuals with dietary
         restrictions, we referred back to the initial research we did and
-        interviewed an additional 2 individuals.
+        interviewed additional individuals.
       </p>
-      <p class="pbody">WIP</p>
-      <p class="pbody">WIP</p>
+      <img src={sofia} alt="Sofia Empathy Map" />
+      <img src={stefano} alt="Stefano Empathy Map" />
+      <img src={lucia} alt="Lucia Empathy Map" />
+      <img src={owen} alt="Owen Empathy Map" />
+      <p class="pbody font-semibold">Contradictions/Tensions</p>
+      <p class="pbody">
+        Interestingly enough, within interviewees there was an interesting
+        dichotemy of tech usage, as diabetics tended to be more reliant on
+        technology than those with Celiac. Additionally, there seemed to be a
+        disconnect between the effort people say they need to track food vs the
+        actual effort.
+      </p>
+      <p class="pbody">
+        For example, Stefano said he could eat anything but also needed to
+        anticipate and track carbs to insure proper insulin levels. He
+        downplayed the impact on daily life but is also hyperdependent on his
+        phone. He used more preventative measures but also said he could
+        basically forget about his condition. Another interviewee stated that
+        they needed to watch what they ate and avoid some restaurants completely
+        while being able to eat out very freely. We thought this may have been
+        due to a tendency to hide his condition/avoid discomfort in group
+        settings.
+      </p>
     </div>
   </div>
 </div>
