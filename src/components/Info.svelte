493f319cac77fc663f52ac47634b4bc9b333f60c
andrewtlu <andrewlulu2012@gmail.com>
2026-02-18 11:54:18 -0500

    Finish P2, make content collapsable

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -6,9 +6,9 @@
 Team info card
  -->
 <div class="flex flex-col gap-1.5">
-  <p class="pbody text-lg">Value Proposition</p>
-  <p class="pbody">Project Intro</p>
-  <p class="pbody text-md text-gray-600 items-center flex gap-1">
+  <!-- <p class="pbody text-lg">Value Proposition</p>
+  <p class="pbody">Project Intro</p> -->
+  <p class="pbody text-base text-gray-600 items-center flex gap-1">
     Built w/ <IconBrandSvelte class="inline-block" /> and <IconHeart
       class="inline-block"
     /> by Sumon Kanti Dey, Andrew Lu, & Julian Lucio Paredes
--- src/components/P1.svelte
+++ src/components/P1.svelte
@@ -1,9 +1,10 @@
 <!-- @component
 P1 component
  -->
-<div class="pcard">
-  <div class="pcard-content">
-    <h1 class="pheader">P1 - Pitch Video/Problem Space</h1>
+<div class="pcard collapse collapse-arrow">
+  <input type="checkbox" name="P1" id="P1" checked />
+  <h1 class="pcard-header collapse-title">P1 - Pitch Video/Problem Space</h1>
+  <div class="pcard-content collapse-content">
     <div class="pcard-body">
       <p class="pbody">
         From our initial interviews, one potential demographic was people with
